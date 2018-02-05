28dd5e90d0a474273b5d5bd1ae86301957155ad4
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-02-05 09:33:00 -0700

    Dashboard and Leaderboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,70 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
-export default class Dashboard extends Component {
-  render() {
+class Dashboard extends Component {
+  state = {
+    showAnswered: false,
+  }
+  showAnswered = () => {
+    this.setState({
+      showAnswered: true,
+    })
+  }
+  showUnanswered = () => {
+    this.setState({
+      showAnswered: false,
+    })
+  }
+  render () {
+    const { showAnswered } = this.state
+    const { answered, unanswered } = this.props
+
+    const list = showAnswered === true
+      ? answered
+      : unanswered
+
     return (
       <div>
-        Dashboard
+        <div className='dashboard-toggle'>
+          <button
+            style={{textDecoration: showAnswered === false ? 'underline' : null}}
+            onClick={this.showUnanswered}>
+              Unanswered
+          </button>
+          <span> | </span>
+          <button
+            style={{textDecoration: showAnswered === true ? 'underline' : null}}
+            onClick={this.showAnswered}>
+              Answered
+          </button>
+        </div>
+        <ul className='dashboard-list'>
+          {list.map((poll) => (
+            <li key={poll.id}>
+              <Link to={`polls/${poll.id}`}>
+                {poll.question}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }
 }
+
+function mapStateToProps ({ authedUser, polls, users }) {
+  const answers = users[authedUser].answers
+
+  const answered = answers.map((id) => polls[id])
+  const unanswered = Object.keys(polls)
+    .filter((qid) => !answers.includes(qid))
+    .map((id) => polls[id])
+
+  return {
+    answered,
+    unanswered,
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard)
--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class Leaderboard extends Component {
+class Leaderboard extends Component {
   render() {
+    const { users } = this.props
+
     return (
-      <div>
-        Leaderboard
-      </div>
+      <ul>
+        {users.map((user) => (
+          <li className='user' key={user.id}>
+            <img src={user.avatarURL} alt={`Avatar for ${user.name}`} />
+            <div>
+              <h1>{user.name}</h1>
+              <p>{user.polls} Polls</p>
+              <p>{user.answers} Answers</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     )
   }
 }
+
+function mapStateToProps ({ users }) {
+  return {
+    users: Object.keys(users)
+      .map((id) => {
+        const { name, avatarURL, polls } = users[id]
+
+        return {
+          id,
+          name,
+          avatarURL,
+          polls: polls.length,
+          answers: users[id].answers.length
+        }
+      })
+      .sort((a, b) => b.polls + b.answers > a.polls + a.answers)
+  }
+}
+
+export default connect(mapStateToProps)(Leaderboard)
