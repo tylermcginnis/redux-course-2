9bdefb7133e3f08c1c62678759f03752aea0c64f
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-02-22 21:36:11 -0700

    Code for project-loading

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,19 @@ import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { receivePolls } from '../actions/polls'
 import { setAuthedUser } from '../actions/authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData () {
   return (dispatch) => {
+    dispatch(showLoading())
     return getInitialData()
       .then(({ users, polls }) => {
         dispatch(receiveUsers(users))
         dispatch(receivePolls(polls))
         dispatch(setAuthedUser(AUTHED_ID))
+        dispatch(hideLoading())
       })
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import authedUser from './authedUser'
 import users from './users'
 import polls from './polls'
+import { loadingBarReducer } from 'react-redux-loading'
 
 export default combineReducers({
   authedUser,
   users,
   polls,
+  loadingBar: loadingBarReducer
 })
