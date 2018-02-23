e8a2f0c04f0de70e42f677ff3d22830ff9703a89
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-02-22 21:20:58 -0700

    Code for project-dashboard

---
+++ src/components/Dashboard.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class Dashboard extends Component {
+  state = {
+    showAnswered: false
+  }
+  showUnaswered = () => {
+    this.setState(() => ({
+      showAnswered: false
+    }))
+  }
+  showAnswered = () => {
+    this.setState(() => ({
+      showAnswered: true
+    }))
+  }
+  render() {
+    const { showAnswered } = this.state
+    const { answered, unanswered } = this.props
+
+    const list = showAnswered === true
+      ? answered
+      : unanswered
+
+    return (
+      <div>
+        <div className='dashboard-toggle'>
+          <button
+            style={{textDecoration: showAnswered === false ? 'underline' : null}}
+            onClick={this.showUnaswered}>
+              Unanswered
+          </button>
+          <span> | </span>
+          <button
+            style={{textDecoration: showAnswered === true ? 'underline' : null}}
+            onClick={this.showAnswered}>
+              Answered
+          </button>
+        </div>
+        <ul className='dashboard-list'>
+          {list.map((poll) => (
+            <li key={poll.id}>
+              {poll.question}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps ({ authedUser, polls, users }) {
+  const answers = users[authedUser].answers
+
+  const answered = answers.map((id) => polls[id])
+    .sort((a,b) => b.timestamp - a.timestamp)
+
+  const unanswered = Object.keys(polls)
+    .filter((id) => !answers.includes(id))
+    .map((id) => polls[id])
+    .sort((a,b) => b.timestamp - a.timestamp)
+
+
+  return {
+    answered,
+    unanswered
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard)
