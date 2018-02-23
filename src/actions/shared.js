27dcc33e7d508186909328a1432a1493de0b796b
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-02-22 20:03:32 -0700

    Code for project-initial-data

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,6 @@
 import { getInitialData } from '../utils/api'
-import { receieveUsers } from '../actions/users'
-import { receievePolls } from '../actions/polls'
+import { receiveUsers } from '../actions/users'
+import { receivePolls } from '../actions/polls'
 import { setAuthedUser } from '../actions/authedUser'
 
 const AUTHED_ID = 'tylermcginnis'
@@ -9,8 +9,8 @@ export function handleInitialData () {
   return (dispatch) => {
     return getInitialData()
       .then(({ users, polls }) => {
-        dispatch(receieveUsers(users))
-        dispatch(receievePolls(polls))
+        dispatch(receiveUsers(users))
+        dispatch(receivePolls(polls))
         dispatch(setAuthedUser(AUTHED_ID))
       })
   }
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
 
 class App extends Component {
+  componentDidMount () {
+    this.props.dispatch(handleInitialData())
+  }
   render() {
     return (
       <div>
@@ -10,4 +15,4 @@ class App extends Component {
   }
 }
 
-export default App
+export default connect()(App)
