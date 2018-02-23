c811bd9eaf0aa686478617a433731ea07da96ff8
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-02-23 16:49:34 -0700

    Code for devtools

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,7 @@
 import logger from './logger'
 import thunk from 'redux-thunk'
-import { applyMiddleware } from 'redux'
+import { applyMiddleware, compose } from 'redux'
 
-export default applyMiddleware(thunk, logger)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+export default composeEnhancers(applyMiddleware(thunk, logger))
