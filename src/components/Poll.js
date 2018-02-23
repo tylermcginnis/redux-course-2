6af44d1e6f8249a5c2dbac9bb9e90b4e9e13fbdf
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-02-23 00:12:27 -0700

    Code for project-poll-ui

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,11 +1,56 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { getPercentage } from '../utils/helpers'
+
+const getVoteKeys = () => ['aVotes', 'bVotes', 'cVotes', 'dVotes']
 
 class Poll extends Component {
+  handleAnswer = (answer) => {
+    const { poll, authedUser } = this.props
+    this.answered = true
+
+    console.log('Add Answer:', answer)
+  }
   render() {
+    if (this.props.poll === null) {
+      return <p>This poll does not exist</p>
+    }
+
+    const { poll, vote, authorAvatar } = this.props
+
+    const totalVotes = getVoteKeys()
+      .reduce((total, key) => total + poll[key].length, 0)
+
     return (
       <div className='poll-container'>
-        {JSON.stringify(this.props)}
+        <h1 className='question'>
+          {poll.question}
+        </h1>
+        <div className='poll-author'>
+          By <img src={authorAvatar} alt="Author's avatar" />
+        </div>
+        <ul>
+          {['aText', 'bText', 'cText', 'dText'].map((key) => {
+            const count = poll[key[0] + 'Votes'].length
+
+            return (
+              <li
+                onClick={() => {
+                  if (vote === null && !this.answered) {
+                    this.handleAnswer(key[0])
+                  }
+                }}
+                className={`option ${vote === key[0] ? 'chosen' : ''}`}>
+                  {vote === null
+                    ? poll[key]
+                    : <div className='result'>
+                        <span>{poll[key]}</span>
+                        <span>{getPercentage(count, totalVotes)}% ({count})</span>
+                      </div>}
+              </li>
+            )
+          })}
+        </ul>
       </div>
     )
   }
@@ -21,7 +66,7 @@ function mapStateToProps ({ authedUser, polls, users}, { match }) {
     }
   }
 
-  const vote = ['aVotes', 'bVotes', 'cVotes', 'dVotes'].reduce((vote, key) => {
+  const vote = getVoteKeys().reduce((vote, key) => {
     if (vote !== null) {
       return vote[0]
     }
